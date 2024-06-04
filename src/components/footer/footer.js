52049968a3a8170ed89eaff6d964b8fa71cf99dc
Ariadna <139250178+AriadnaLZ@users.noreply.github.com>
2024-06-04 11:31:43 +0200

    Add files via upload

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,6 +4,7 @@ export const Footer = () => {
   const footer = document.querySelector('footer')
   const divFooter = document.createElement('div')
   footer.append(divFooter)
+  divFooter.className = 'divFooter'
   const pFooter = document.createElement('p')
   pFooter.innerText = '©️ Created by Ariadna Lopez'
   divFooter.append(pFooter)
